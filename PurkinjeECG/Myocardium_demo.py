49e41aca72359d41d99dd49f1e4384eab65fc13f
Simone Pezzuto <simone.pezzuto@unitn.it>
2025-01-14 16:29:55 +0100

    Update tetra code

--- PurkinjeECG/Myocardium_demo.py
+++ PurkinjeECG/Myocardium_demo.py
@@ -14,14 +14,16 @@ import matplotlib.pyplot as plt
 
 from scipy import sparse
 
-def Bmatrix(nodeCoords):
+def Bmatrix(pts,elm):
+
+    nodeCoords = np.moveaxis(pts[elm,:],0,-1)
 
     # NOTE: The definition of the parent tetrahedron by Hughes - "The Finite Element Method" (p. 170) is different from the 
     # local order given by VTK (http://victorsndvg.github.io/FEconv/formats/vtk.xhtml) 
     # Here we follow the VTK convention
-    x1 = nodeCoords[0][0]; x2 = nodeCoords[1][0]; x3 = nodeCoords[2][0]; x4 = nodeCoords[3][0]
-    y1 = nodeCoords[0][1]; y2 = nodeCoords[1][1]; y3 = nodeCoords[2][1]; y4 = nodeCoords[3][1]
-    z1 = nodeCoords[0][2]; z2 = nodeCoords[1][2]; z3 = nodeCoords[2][2]; z4 = nodeCoords[3][2]
+    x1 = nodeCoords[0,0,:]; x2 = nodeCoords[1,0,:]; x3 = nodeCoords[2,0,:]; x4 = nodeCoords[3,0,:]
+    y1 = nodeCoords[0,1,:]; y2 = nodeCoords[1,1,:]; y3 = nodeCoords[2,1,:]; y4 = nodeCoords[3,1,:]
+    z1 = nodeCoords[0,2,:]; z2 = nodeCoords[1,2,:]; z3 = nodeCoords[2,2,:]; z4 = nodeCoords[3,2,:]
 
     x14 = x1 - x4; x34 = x3 - x4; x24 = x2 - x4
     y14 = y1 - y4; y34 = y3 - y4; y24 = y2 - y4
@@ -33,21 +35,23 @@ def Bmatrix(nodeCoords):
     Jinv_21 = -1. * (y14 * z24 - y24 * z14); Jinv_22 =        x14 * z24 - x24 * z14 ; Jinv_23 = -1. * (x14 * y24 - x24 * y14)
     Jinv_31 =        y14 * z34 - y34 * z14 ; Jinv_32 = -1. * (x14 * z34 - x34 * z14); Jinv_33 =        x14 * y34 - x34 * y14
 
-    B_def = np.array([[1., 0., 0., -1.],
-                        [0., 0., 1., -1.],
-                        [0., 1., 0., -1.]])
-    
     Jinv = np.array([[Jinv_11, Jinv_12, Jinv_13],
-                        [Jinv_21, Jinv_22, Jinv_23],
-                        [Jinv_31, Jinv_32, Jinv_33]])
-    
-    B = np.dot(Jinv.T,B_def)
+                     [Jinv_21, Jinv_22, Jinv_23],
+                     [Jinv_31, Jinv_32, Jinv_33]])
+
+    B_def = np.array([[1., 0., 0., -1.],
+                      [0., 0., 1., -1.],
+                      [0., 1., 0., -1.]])
+
+    B = Jinv.T @ B_def[...,:]
 
     return B, detJ
 
-def StiffnessMatrix(B,J, G = np.eye(3)):
-    # stiffness matrix for tetrahedra
-    return np.dot(B.T, np.dot(G,B)) / (6. * J)
+def localStiffnessMatrix(B,J,G):
+    "Assemble the local stiffness matrix"
+    K = np.einsum('nji,njk,nkl->nil',B,G,B) / 6.0
+    K = 1/J[:,None,None] * K
+    return K
 
 class MyocardialMesh:
     "VTK mesh of endocardium (left, right or both)"
@@ -205,24 +209,19 @@ class MyocardialMesh:
         self.fim     = FIMPY.create_fim_solver(self.xyz, cells, self.D, device = 'gpu')
         print(time.time() - start_time)
 
-    def assemble_K(self, xyz, cells, Gi):
-        # Gi is required at the cells
-        I,J,Vk = [],[],[]
-        for k,tri in enumerate(cells):
-            j, i = np.meshgrid(tri,tri)
-            I.extend(list(i.ravel()))
-            J.extend(list(j.ravel()))
-            B, Jac = Bmatrix(xyz[tri])
-            Kloc = StiffnessMatrix(B,Jac, Gi[k])
+    def assemble_K(self,pts,elm,Gi):
 
-            Vk.extend(list(Kloc.ravel()))
+        B, J = Bmatrix(pts,elm)
+        K = localStiffnessMatrix(B,J,Gi)
+        N = pts.shape[0]
+        Kvals = K.ravel('C')
 
-        n = xyz.shape[0]
-        K = sparse.coo_matrix((Vk,(I,J)),shape=(n,n)).tocsr()
+        II = np.repeat(elm, 4, axis=1).ravel()
+        JJ = np.tile(elm, 4).ravel()
 
-        return K
+        Kp = sparse.coo_matrix((Kvals,(II,JJ)),shape=(N,N)).tocsr()
 
-
+        return Kp
 
     def find_closest_pmjs(self, pmjs):
         "Project PMJs from the tree onto the cell mesh"
